6a99f2f3b3b947b7658c84cc678620edfa45d3d5
Michael Dotson <thedotson@icloud.com>
2020-08-17 19:50:20 -0500

    created auth component

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import BoardContainer from '../components/BoardContainer/BoardContiner';
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
+    const loadComponent = () => {
+      if (authed) {
+        return <BoardContainer />;
+      }
+
+      return <Auth />;
+    };
+
     return (
       <div className="App">
         <h1>React-Pinterest</h1>
-        <button className="btn btn-danger">
-        <i className="fab fa-pinterest"></i>Pinterest Button<i className="fab fa-pinterest"></i></button>
+        <MyNavbar />
+        {loadComponent()}
       </div>
     );
   }
---
+++ src/components/BoardContainer/BoardContiner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BoardContainer extends React.Component {
+  render() {
+    return (
+      <div className="BoardContainer">
+        <h1>Hi, i'm board container</h1>
+      </div>
+    );
+  }
+}
+
+export default BoardContainer;
