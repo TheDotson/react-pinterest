45931106e6cb33fffa28146bd4b0794f2da5c3bc
Michael Dotson <thedotson@icloud.com>
2020-08-22 09:37:41 -0500

    added working delete function for pins

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import pinShape from '../../helpers/props/pinShape';
 
 class Pin extends React.Component {
   static propTypes = {
     pin: pinShape.pinShape,
+    deletePin: PropTypes.func.isRequired,
+  }
+
+  deletePinEvent = (e) => {
+    e.preventDefault();
+    const { pin, deletePin } = this.props;
+    deletePin(pin.id);
   }
 
   render() {
@@ -13,6 +21,7 @@ class Pin extends React.Component {
       <div className="card border-0 pins">
         <img className="card-img pin-image" src={pin.imageUrl} alt={pin.title} />
         <div className="card-img-overlay">
+          <button className="btn btn-danger" onClick={this.deletePinEvent}>Delete Pin</button>
           <h5 className="card-title">{pin.title}</h5>
         </div>
       </div>
