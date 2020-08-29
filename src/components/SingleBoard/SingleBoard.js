ff3c4b64926e7d5331ba9c37a6e241566a9fe5be
Michael Dotson <thedotson@icloud.com>
2020-08-29 08:56:43 -0500

    edit pin works

--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -15,6 +15,7 @@ class SingleBoard extends React.Component {
     board: {},
     pins: [],
     formOpen: false,
+    editPin: {},
   }
 
   goGetYoPins = () => {
@@ -27,7 +28,6 @@ class SingleBoard extends React.Component {
 
   componentDidMount() {
     const { boardId } = this.props;
-
     boardsData.getSingleBoard(boardId)
       .then((response) => this.setState({ board: response.data }))
       .catch((err) => console.error('get single board failed', err));
@@ -37,9 +37,7 @@ class SingleBoard extends React.Component {
 
   deletePin = (pinId) => {
     pinsData.deletePin(pinId)
-      .then(() => {
-        this.goGetYoPins();
-      })
+      .then(() => { this.goGetYoPins(); })
       .catch((err) => console.error('delete pin failed', err));
   };
 
@@ -52,16 +50,37 @@ class SingleBoard extends React.Component {
       .catch((err) => console.error('Create pin failed', err));
   }
 
-  render() {
-    const { board, pins, formOpen } = this.state;
-    const { setSingleBoard, boardId } = this.props;
+  editAPin = (pinToEdit) => {
+    this.setState({ formOpen: true, editPin: pinToEdit });
+  }
 
-    const pinCards = pins.map((pin) => <Pin key={pin.id} pin={pin} deletePin={this.deletePin}/>);
+  updatePin = (pinId, editedPin) => {
+    pinsData.updatePin(pinId, editedPin)
+      .then(() => {
+        this.goGetYoPins();
+        this.setState({ formOpen: false, editPin: {} });
+      })
+      .catch((err) => console.error('Update pin failed', err));
+  }
+
+  closeForm = () => {
+    this.setState({ formOpen: false });
+  }
+
+  render() {
+    const {
+      board,
+      pins,
+      formOpen,
+      editPin,
+    } = this.state;
+    const { setSingleBoard, boardId } = this.props;
+    const pinCards = pins.map((pin) => <Pin key={pin.id} pin={pin} setSingleBoard={setSingleBoard} deletePin={this.deletePin} editAPin={this.editAPin}/>);
 
     return (
       <div>
         <button className="btn btn-warning" onClick={() => { this.setState({ formOpen: !formOpen }); }}><i className="far fa-plus-square"></i></button>
-        { formOpen ? <PinForm boardId={boardId} createPin={this.createPin}/> : '' }
+        { formOpen ? <PinForm boardId={boardId} createPin={this.createPin} editPin={editPin} updatePin={this.updatePin} closeForm={this.closeForm}/> : '' }
         <h4>{board.name}</h4>
         <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>X</button>
         <div className="card-columns">
